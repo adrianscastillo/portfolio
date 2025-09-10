c5315a47ef93a8ce36bb540f79d4ea59131f0f81
Adrian <Adrian@MacBookPro.lan>
2025-09-09 18:08:18 -0700

    Add CNAME file for custom domain and update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 const isPreview = process.argv.some(arg => arg.includes('preview'))
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' || isPreview ? '/portfolio/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
